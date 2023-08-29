fb80e2582750b2bd6b9038a31d791075a22ed3ac
maiones <twincky@yandex.ru>
2023-08-29 13:08:17 +0300

    Переделал с esac

--- wine_pain_1.sh
+++ wine_pain_1.sh
@@ -4,27 +4,24 @@ LANG=C
 echo -n "IP мед пк: "
 read inputval
 
-echo -n "Удалить префикс wine и починить все что можно 'pupa'; удалить пакет wine 'lupa'; ядро 5.18.14 'oreh'; wine 4.9 не пашет word 'woo': "
-read inputval1
+read -p "Удалить префикс wine и починить все что можно 'pupa'; удалить пакет wine 'lupa'; ядро 5.18.14 'oreh'; wine 4.9 не пашет word 'woo': " decision1 
 
-if test "$inputval1" == "pupa"
-then
-	ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_2.sh
-fi
-
-if test "$inputval1" == "lupa"
-then
-	ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_3.sh
-fi
-
-if test "$inputval1" == "oreh"
-then
-	ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_4.sh
-fi
-
-if test "$inputval1" == "woo"
-then
-   scp -o "StrictHostKeyChecking=no" -i /home/user/medkey -r /home/user/kva-kva/med_linux/word/Star_wine.tar.gz root@$inputval:/tmp/
-	ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_5.sh
-fi
+case "$decision1" in
+	"pupa")
+		ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_2.sh
+		;;
+	"lupa")
+		ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_3.sh
+		;;
+	"oreh")
+		ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_4.sh
+		;;
+	"woo")
+		scp -o "StrictHostKeyChecking=no" -i /home/user/medkey -r /home/user/kva-kva/med_linux/word/Star_wine.tar.gz root@$inputval:/tmp/
+		ssh -o "StrictHostKeyChecking=no" -i /home/user/medkey root@$inputval 'bash -s' < /home/user/kva-kva/scripts/wine_pain_5.sh
+		;;
+	*)
+		 echo "Некорректный ввод"
+		 ;;
+esac
 
